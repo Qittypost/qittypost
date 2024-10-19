94a9191853d25471fef91758bc64b6740280d5e7
Qittypost <forworkpro4@gmail.com>
2024-10-19 14:11:46 +0300

    Update privacy.js

--- privacy.js
+++ privacy.js
@@ -3,6 +3,19 @@ const menu = header.querySelector('p');
 
 let isexpanded = false;
 
+try {
+    const tg = window.Telegram.WebApp;
+    if (tg) {
+        tg.ready();
+        tg.disableVerticalSwipes();
+        tg.expand();
+    } else {
+        console.log('Tg web app api is\'n initialized');
+    }
+} catch(e) {
+    console.log('Error', e);
+}
+
 menu.addEventListener('click', () => {
     if (isexpanded) {
         header.style.height = '90px';
