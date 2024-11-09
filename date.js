ef314a94a9352090884982495585dea9a8fdfee6
Qittypost <forworkpro4@gmail.com>
2024-11-09 13:20:24 +0200

    a

---
+++ date.js
@@ -0,0 +1,42 @@
+const date = document.querySelector('[type="date"]');
+const time = document.querySelector('[type="time"]');
+const submit = document.getElementById('submit');
+
+const date_inp = document.getElementById('date');
+const time_inp = document.getElementById('time');
+
+const urlParams = new URLSearchParams(window.location.search);
+const tg = window.Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+tg.disableVerticalSwipes();
+
+let d = urlParams.get('date');
+let t = urlParams.get('time');
+
+const today = new Date();
+
+date.value = date_inp.value = d ? d : `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`; 
+time.value = time_inp.value = t ? t : `${today.getHours()}:${today.getMinutes()}`;
+
+
+date_inp.addEventListener('click', (e) => {
+    date.style.top = `${e.clientY}px`;
+    date.style.left = `${e.clientX}px`;
+    setTimeout(() => date.showPicker());
+});
+
+date.addEventListener('change', () => {
+    date_inp.value = date.value;
+});
+
+time_inp.addEventListener('click', (e) => {
+    time.style.top = `${e.clientY}px`;
+    time.style.left = `${e.clientX}px`;
+    setTimeout(() => time.showPicker());
+});
+
+time.addEventListener('change', () => {
+    time_inp.value = time.value;
+});
